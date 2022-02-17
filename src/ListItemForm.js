693cf6310a0bc71097054bc37067b9b34908baa0
CarolynnFleming <cfcatpink@gmail.com>
2022-02-16 22:26:50 -0500

    handle submit

--- src/ListItemForm.js
+++ src/ListItemForm.js
@@ -10,11 +10,13 @@ export default function ListItemForm({ fetchItems }) {
     e.preventDefault();
     
     // make a new list item in supabase using the form values stored in state
-    
+    await createListItem(name, quantity);
 
     // refetch the items using the handler functionpassed down as a prop
-
+    await fetchItems();
     // clear the name and quantity in state to refresh the form
+    setName('');
+    setQuantity('');
   }
 
   return (
